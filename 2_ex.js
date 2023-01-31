d25f90af612a42d06d5c6e80933a74ee58560e89
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 16:09:16 +0300

    2_ex.js (full done)

--- 2_ex.js
+++ 2_ex.js
@@ -1,16 +1,25 @@
 let a = 13.890123;
 let b = 2.891564;
+let n = 3;
 
-let num1 = a % 1;
-let num2 = b % 1;
+let num1 = a - Math.trunc(a);
+let num2 = b - Math.trunc(b);
 
-console.log(num1.toFixed(3));
-console.log(num2.toFixed(3));
+let num01 = Math.trunc(num1 * 10 ** n);
+let num02 = Math.trunc(num2 * 10 ** n);
 
-//сравнения
-console.log(num1 > num2);
-console.log(num1 < num2);
-console.log(num1 >= num2);
-console.log(num1 <= num2);
-console.log(num1 === num2);
-console.log(num1 != num2);
+console.log("Дробная часть числа a:", num01);
+console.log("Дробная часть числа b:", num02);
+//let num1 = a % 1;
+//let num2 = b % 1;
+
+//console.log(num1.toFixed(3));
+//console.log(num2.toFixed(3));
+
+console.log("Сравнение:");
+console.log("890 > 891", num1 > num2);
+console.log("890 < 891", num1 < num2);
+console.log("890 >= 891", num1 >= num2);
+console.log("890 <= 891", num1 <= num2);
+console.log("890 = 891", num1 === num2);
+console.log("890 != 891", num1 != num2);
