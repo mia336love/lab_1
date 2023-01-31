aa1ba1ba9bfb7148e7df655c6bb700b403bb24f3
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 09:20:23 +0300

    1_ex.js (all done)

---
+++ 1_ex.js
@@ -0,0 +1,11 @@
+const x1 = 2;
+const y1 = 3;
+
+const x2 = 10;
+const y2 = 5;
+
+const side1 = x2 - x1;
+const side2 = y2 - y1;
+
+area = side1 * side2;
+console.log("Площадь прямоугольника равна", area);
