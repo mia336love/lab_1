7594bf4802ad82ec67802b70ef6cd253bc120f50
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 09:39:27 +0300

    3_ex.js (odd numbers // full done)

--- 3_ex.js
+++ 3_ex.js
@@ -1,6 +1,9 @@
 const n = 0;
 const m = 100;
 
-console.log(
-  Math.round(Math.random() * (Math.max(n, m) - Math.min(n, m)) + Math.min(n, m))
+const num = Math.round(
+  Math.random() * (Math.max(n, m) - Math.min(n, m)) + Math.min(n, m)
 );
+
+const num1 = num + (num % 2) + 1;
+console.log(num1);
