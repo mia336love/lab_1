5149de2704bfda9fcc770c30b9b3b77f5a7e331c
mia336love <122865270+mia336love@users.noreply.github.com>
2023-01-31 08:42:07 +0300

    2_ex (done)

---
+++ 2_ex.js
@@ -0,0 +1,11 @@
+let a = 13.890123;
+let b = 2.891564;
+
+//console.log(a.toFixed(2) % 1);
+//console.log(b.toFixed(2) % 1);
+
+let num1 = a % 1;
+let num2 = b % 1;
+
+console.log(num1.toFixed(2));
+console.log(num2.toFixed(2));
